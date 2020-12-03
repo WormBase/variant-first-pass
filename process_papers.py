e8d73292f65d5209e181d8258f241da9b519218b
Valerio Arnaboldi <valearna@caltech.edu>
2020-12-03 10:31:03 -0800

    feat(*) added function to send out emails
    #1

--- process_papers.py
+++ process_papers.py
@@ -1,19 +1,39 @@
 import argparse
 import logging
 import re
+import smtplib
+from datetime import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-from wbtools.db.dbmanager import WBDBManager
 from wbtools.db.generic import WBGenericDBManager
-from wbtools.lib.nlp import get_new_variations_from_text, NEW_VAR_REGEX, PaperSections
+from wbtools.lib.nlp import get_new_variations_from_text, PaperSections
 from wbtools.literature.corpus import CorpusManager
-from wbtools.lib.scraping import get_cgc_allele_designations
+
 
 logger = logging.getLogger(__name__)
 
 
+def send_email(subject, content, recipients, server_host, server_port, email_user, email_passwd):
+    body = MIMEText(content, "html")
+    msg = MIMEMultipart('alternative')
+    msg.attach(body)
+    msg['Subject'] = subject
+    msg['From'] = "outreach@wormbase.org"
+    msg['To'] = ", ".join(recipients)
+
+    try:
+        server_ssl = smtplib.SMTP_SSL(server_host, server_port)
+        server_ssl.login(email_user, email_passwd)
+        server_ssl.send_message(msg)
+        logger.info("Email sent to: " + ", ".join(recipients))
+        server_ssl.quit()
+    except:
+        logger.fatal("Can't connect to smtp server. Email not sent.")
+
+
 def main():
-    parser = argparse.ArgumentParser(description="Build corpus from WB papers and calculate similarity between "
-                                                 "articles")
+    parser = argparse.ArgumentParser(description="Variant First Pass")
     parser.add_argument("-N", "--db-name", metavar="db_name", dest="db_name", type=str)
     parser.add_argument("-U", "--db-user", metavar="db_user", dest="db_user", type=str)
     parser.add_argument("-P", "--db-password", metavar="db_password", dest="db_password", type=str, default="")
@@ -27,9 +47,15 @@ def main():
     parser.add_argument("-L", "--log-level", dest="log_level", choices=['DEBUG', 'INFO', 'WARNING', 'ERROR',
                                                                         'CRITICAL'], default="INFO",
                         help="set the logging level")
-    parser.add_argument("-m", "--model", metavar="model_path", dest="model_path", type=str)
     parser.add_argument("-d", "--from-date", metavar="from_date", dest="from_date", type=str,
                         help="use only articles included in WB at or after the specified date")
+    parser.add_argument("-o", "--email-host", metavar="email_host", dest="email_host", type=str)
+    parser.add_argument("-r", "--email-port", metavar="email_port", dest="email_port", type=int)
+    parser.add_argument("-u", "--email-user", metavar="email_user", dest="email_user", type=str)
+    parser.add_argument("-p", "--email-password", metavar="email_password", dest="email_password", type=str)
+    parser.add_argument("-e", "--email-recipient", metavar="email_recipient", dest="email_recipient", type=str)
+    parser.add_argument("-t", "--testing", action="store_true")
+
     args = parser.parse_args()
     logging.basicConfig(filename=args.log_file, level=args.log_level,
                         format='%(asctime)s - %(name)s - %(levelname)s:%(message)s')
@@ -37,31 +63,37 @@ def main():
     cm = CorpusManager()
     cm.load_from_wb_database(args.db_name, args.db_user, args.db_password, args.db_host,
                              tazendra_ssh_user=args.tazendra_ssh_user, tazendra_ssh_passwd=args.tazendra_ssh_password,
-                             from_date=args.from_date, load_curation_info=True, max_num_papers=10,
-                             paper_ids=["00054758", "00054762", "00054766", "00054772", "00054777", "00054789",
-                                        "00054790", "00054792"])
-    db_manager = WBGenericDBManager(dbname=args.db_name, user=args.db_user, password=args.db_password, host=args.db_host)
-    curated_alleles = db_manager.get_curated_variations(exclude_id_used_as_name=True)
-    print("PAPER_ID", "ALLELE_NAME", "TYPE", "SVM_VALUE", "NUM_MATCHES_IN_SENTENCE", "SENTENCE", sep="\t")
-    for paper in cm.corpus.values():
-        full_text = " ".join(paper.get_text_docs(include_supplemental=True, remove_sections=[PaperSections.REFERENCES],
-                                                 must_be_present=[PaperSections.RESULTS],
-                                                 split_sentences=False, lowercase=False, tokenize=False,
-                                                 remove_stopwords=False, remove_alpha=False))
-        sentences = paper.get_text_docs(include_supplemental=True, remove_sections=[PaperSections.REFERENCES],
-                                        must_be_present=[PaperSections.RESULTS],
-                                        split_sentences=True, lowercase=False, tokenize=False, remove_stopwords=False,
-                                        remove_alpha=False)
-        svm_value = paper.svm_values["seqchange"]
-        extracted_alleles = get_new_variations_from_text(full_text)
-        extracted_alleles = [allele for allele in extracted_alleles if allele[0] not in curated_alleles]
-        if svm_value:
+                             from_date=args.from_date)
+    if cm.size() > 0:
+        db_manager = WBGenericDBManager(dbname=args.db_name, user=args.db_user, password=args.db_password,
+                                        host=args.db_host)
+        curated_alleles = db_manager.get_curated_variations(exclude_id_used_as_name=True)
+        results = "PAPER_ID&emsp;VARIANT_NAME&emsp;TYPE&emsp;SVM_VALUE&emsp;NUM_MATCHES_IN_PAPER&emsp;MATCHED_SENTENCES<br/><br/>"
+        for paper in cm.corpus.values():
+            full_text = " ".join(paper.get_text_docs(include_supplemental=True, remove_sections=[PaperSections.REFERENCES],
+                                                     must_be_present=[PaperSections.RESULTS],
+                                                     split_sentences=False, lowercase=False, tokenize=False,
+                                                     remove_stopwords=False, remove_alpha=False))
+            sentences = paper.get_text_docs(include_supplemental=True, remove_sections=[PaperSections.REFERENCES],
+                                            must_be_present=[PaperSections.RESULTS],
+                                            split_sentences=True, lowercase=False, tokenize=False,
+                                            remove_stopwords=False, remove_alpha=False)
+            svm_value = paper.svm_values["seqchange"] if paper.svm_values["seqchange"] else "not_yet_classified"
+            extracted_alleles = get_new_variations_from_text(full_text)
+            extracted_alleles = [allele for allele in extracted_alleles if allele[0] not in curated_alleles]
+
             for allele, suspicious in extracted_alleles:
                 allele_regex = r".*[ \(]" + allele + r"[ \)\[].*"
-                matching_sentences = [sentence for sentence in sentences if re.match(allele_regex, sentence)]
-                for matching_sentence in matching_sentences:
-                    count = len(re.findall(allele_regex, matching_sentence))
-                    print(paper.paper_id, allele, suspicious, svm_value, count, matching_sentence, sep="\t")
+                matching_sentences = ",".join(["\"" + sentence + "\"" for sentence in sentences if
+                                               re.match(allele_regex, sentence)])
+                count = len(re.findall(allele_regex, matching_sentences))
+                results += "&emsp;".join([paper.paper_id, allele, suspicious, svm_value, str(count), matching_sentences]) + \
+                           "<br/>"
+
+        send_email(subject=("[Test] " if args.testing else "") + "[Variant First Pass] Results from " + args.from_date +
+                                                                 " to " + str(datetime.now().strftime('%Y-%m-%d')),
+                   content=results, recipients=[args.email_recipient], server_host=args.email_host,
+                   server_port=args.email_port, email_user=args.email_user, email_passwd=args.email_password)
 
 
 if __name__ == '__main__':
