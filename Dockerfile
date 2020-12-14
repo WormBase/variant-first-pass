e3efe4c81112e071f774d0c911c68528d4c9c218
Valerio Arnaboldi <valearna@caltech.edu>
2020-12-14 13:56:02 -0800

    feat(*) added dockerfile and crontab file
    #1

---
+++ Dockerfile
@@ -0,0 +1,49 @@
+FROM python:3.8-slim
+
+RUN apt-get update && apt-get install -y cron
+
+WORKDIR /usr/src/app/
+ADD requirements.txt .
+RUN pip3 install -r requirements.txt
+COPY . .
+
+ENV DB_HOST=""
+ENV DB_NAME=""
+ENV DB_USER=""
+ENV DB_PASSWD=""
+ENV MAX_NUM_PAPERS=50
+ENV TAZENDRA_USER=""
+ENV TAZENDRA_PASSWD=""
+ENV FROM_DATE="1970-01-01"
+ENV EMAIL_USER=""
+ENV EMAIL_HOST=""
+ENV EMAIL_PORT=""
+ENV EMAIL_PASSWD=""
+ENV EMAIL_RECIPIENT=""
+ENV EXCLUDE_IDS_FILE=""
+ENV EXCLUDE_PAP_TYPES=""
+
+
+ADD crontab /etc/cron.d/vfp-cron
+RUN chmod 0644 /etc/cron.d/vfp-cron
+RUN touch /var/log/vfp_pipeline.log
+RUN crontab /etc/cron.d/vfp-cron
+
+ENV PYTHONPATH=$PYTHONPATH:/usr/src/app/
+
+CMD echo $DB_HOST > /etc/vfp_db_host && \
+    echo $DB_NAME > /etc/vfp_db_name && \
+    echo $DB_USER > /etc/vfp_db_user && \
+    echo $DB_PASSWD > /etc/vfp_db_passwd && \
+    echo $MAX_NUM_PAPERS > /etc/vfp_max_num_papers && \
+    echo $TAZENDRA_USER > /etc/vfp_tazendra_user && \
+    echo $TAZENDRA_PASSWD > /etc/vfp_tazendra_passwd && \
+    echo $FROM_DATE > /etc/vfp_from_date && \
+    echo $EMAIL_USER > /etc/vfp_email_user && \
+    echo $EMAIL_HOST > /etc/vfp_email_host && \
+    echo $EMAIL_PORT > /etc/vfp_email_port && \
+    echo $EMAIL_PASSWD > /etc/vfp_email_passwd && \
+    echo $EMAIL_RECIPIENT > /etc/vfp_email_recipient && \
+    echo $EXCLUDE_IDS_FILE > /etc/vfp_exclude_ids_file && \
+    echo $EXCLUDE_PAP_TYPES > /etc/vfp_exclude_pap_types && \
+    cron && tail -f /dev/null
--- process_papers.py
+++ process_papers.py
@@ -5,6 +5,7 @@ import smtplib
 from datetime import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from pathlib import Path
 
 from wbtools.db.generic import WBGenericDBManager
 from wbtools.lib.nlp import get_new_variations_from_text, PaperSections
@@ -56,15 +57,23 @@ def main():
     parser.add_argument("-e", "--email-recipient", metavar="email_recipient", dest="email_recipient", type=str)
     parser.add_argument("-t", "--testing", action="store_true")
     parser.add_argument("-m", "--max-num-papers", metavar="max_num_papers", dest="max_num_papers", type=int)
+    parser.add_argument("-x", "--exclude-ids-file", metavar="exclude_ids_file", dest="exclude_ids_file", type=str,
+                        default=None)
+    parser.add_argument("-y", "--exclude-pap-types", metavar="exclude_pap_types", dest="exclude_pap_types", nargs="*",
+                        type=str, default=None)
 
     args = parser.parse_args()
     logging.basicConfig(filename=args.log_file, level=args.log_level,
                         format='%(asctime)s - %(name)s - %(levelname)s:%(message)s')
 
+    if args.exclude_ids_file:
+        Path(args.exclude_ids_file).touch()
     cm = CorpusManager()
     cm.load_from_wb_database(args.db_name, args.db_user, args.db_password, args.db_host,
                              tazendra_ssh_user=args.tazendra_ssh_user, tazendra_ssh_passwd=args.tazendra_ssh_password,
-                             from_date=args.from_date, max_num_papers=args.max_num_papers, must_have_svm=True)
+                             from_date=args.from_date, max_num_papers=args.max_num_papers,
+                             exclude_ids=[line.strip() for line in open(args.exclude_ids_file)] if args.exclude_ids_file
+                             else None, must_have_svm=True, exclude_pap_types=args.exclude_pap_types)
     remove_sections = [PaperSections.INTRODUCTION, PaperSections.REFERENCES]
     must_be_present = [PaperSections.RESULTS]
     if cm.size() > 0:
@@ -98,8 +107,11 @@ def main():
                                           f"kbox_pdf=on&action=Get+Results\">{paper.paper_id}</a>",
                                           allele, suspicious, svm_value, str(count), matching_sentences]) + "<br/>"
 
-        send_email(subject=("[Test] " if args.testing else "") + "[Variant First Pass] Results from " + args.from_date +
-                                                                 " to " + str(datetime.now().strftime('%Y-%m-%d')),
+        with open(args.exclude_ids_file, 'a') as exclude_ids_file:
+            for paper in cm.get_all_papers():
+                exclude_ids_file.write(paper.paper_id + "\n")
+
+        send_email(subject=("[Test] " if args.testing else "") + "[Variant First Pass] New Results",
                    content=results, recipients=[args.email_recipient], server_host=args.email_host,
                    server_port=args.email_port, email_user=args.email_user, email_passwd=args.email_password)
 
